3e05b7f72c23eeb8ccba34c6a04f74110d042806
Donny <donthephan@gmail.com>
2021-12-01 19:17:50 +0000

    moved setup and inputhandler to input.js

--- client.js
+++ client.js
@@ -30,24 +30,6 @@ const connect = function() {
   return conn;
 };
 
-// setup interface to handle user input from stdin
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
 
-  stdin.on('data', handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    console.log('EXITING')
-    process.exit();
-  }
-};
-
-module.exports = { connect, setupInput };
+module.exports = { connect };
