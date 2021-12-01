a4832d8a92d0af9cbaec1d7df6994aece4a78ef7
Donny <donthephan@gmail.com>
2021-12-01 19:11:14 +0000

    add setupInput and handleUserInput

--- client.js
+++ client.js
@@ -23,11 +23,31 @@ const connect = function() {
   //   // code that does something when the connection is first established
   // });
 
-  process.stdin.on('data', function(message) {
-    conn.write('Move: up');
-  });
+  // process.stdin.on('data', function(message) {
+  //   conn.write('Move: up');
+  // });
 
   return conn;
 };
 
-module.exports = { connect };
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    console.log('EXITING')
+    process.exit();
+  }
+};
+
+module.exports = { connect, setupInput };
