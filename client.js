3859f280aa177aa10debed72049877f0b2eed966
Donny <donthephan@gmail.com>
2021-12-01 18:43:46 +0000

    moved connect function to client.js

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require('net');
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
+  // conn.on("connect", () => {
+  //   // code that does something when the connection is first established
+  // });
+
+  process.stdin.on('data', function(message) {
+    conn.write(message);
+  });
+
+
+  return conn;
+};
+
+module.exports = { connect }
